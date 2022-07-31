1a24e83eedf3f4f3107b0679964583e04d995541
vn018 <108856806+vn018@users.noreply.github.com>
2022-07-31 11:40:15 +0530

    Add files via upload

---
+++ Authorization-Okta/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Okta.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth1.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            if (!HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Challenge(OktaDefaults.MvcAuthenticationScheme);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult SignOut()
+        {
+            return new SignOutResult(
+                    new[]
+                    {
+        OktaDefaults.MvcAuthenticationScheme,
+        CookieAuthenticationDefaults.AuthenticationScheme,
+                    },
+                    new AuthenticationProperties { RedirectUri = "/Home/" });
+        }
+    }
+}
